984bd51982718b7558f21a88ec7508f2ff27ae38
Tony Pan <tcp1975@gmail.com>
2020-06-24 13:44:44 -0400

    REFACT: rename error_handler to report.hpp

--- include/utils/report.hpp
+++ include/utils/report.hpp
@@ -18,6 +18,17 @@ void print_err(Types... args) {
     fprintf(stderr, args...);
 }
 
+template <class... Types>
+void print(Types... args) {
+#ifdef WITH_MPI
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == 0) fprintf(stdout, args...);
+#else
+    fprintf(stdout, args...);
+#endif
+}
+
 
 
 } }
