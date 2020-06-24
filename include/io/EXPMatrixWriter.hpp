ca591fb7c35ddd842597b029a40419c87fea919c
Tony Pan <tcp1975@gmail.com>
2020-06-24 13:56:17 -0400

    ENH: added methods to write from aligned_matrix.

--- include/io/EXPMatrixWriter.hpp
+++ include/io/EXPMatrixWriter.hpp
@@ -17,7 +17,8 @@
 #include <string>
 #include <limits>
 
-#include "benchmark.hpp"
+#include "utils/benchmark.hpp"
+#include "ds/aligned_matrix.hpp"
 
 #ifndef EXP_SKIP_TWO_ROWS
 #define EXP_SKIP_TWO_ROWS false
@@ -31,13 +32,13 @@ class EXPMatrixWriter {
 public:
 
     static bool storeMatrixData(const char * fileName, size_t const & rows, size_t const & cols, 
-        FloatType* vectors, int const & vectorSizeAligned, bool const & skip = EXP_SKIP_TWO_ROWS) {
-            return EXPMatrixWriter::storeMatrixData(std::string(fileName), rows, cols, vectors, vectorSizeAligned, skip);
+        FloatType* vectors, size_t const & stride_bytes, bool const & skip = EXP_SKIP_TWO_ROWS) {
+            return EXPMatrixWriter::storeMatrixData(std::string(fileName), rows, cols, vectors, stride_bytes, skip);
         }
 
 
     static bool storeMatrixData(std::string const & fileName, size_t const & rows, size_t const & cols,
-        FloatType* vectors, int const & vectorSizeAligned, bool const & skip = EXP_SKIP_TWO_ROWS) {
+        FloatType* vectors, size_t const & stride_bytes, bool const & skip = EXP_SKIP_TWO_ROWS) {
         
 		std::vector<std::string> row_names(rows);
 		for (size_t i = 0; i < rows; ++i) {
@@ -50,19 +51,40 @@ public:
 		return EXPMatrixWriter::storeMatrixData(fileName, 
 			row_names,
 			col_names,
-			vectors, vectorSizeAligned, skip);
+			vectors, stride_bytes, skip);
+    }
+
+    static bool storeMatrixData(const char * fileName, 
+        splash::ds::aligned_matrix<FloatType> const & input, bool const & skip = EXP_SKIP_TWO_ROWS) {
+        return EXPMatrixWriter::storeMatrixData(std::string(fileName), input, skip);
+    }
+    static bool storeMatrixData(std::string const & fileName, 
+        splash::ds::aligned_matrix<FloatType> const & input, bool const & skip = EXP_SKIP_TWO_ROWS) {
+        
+		std::vector<std::string> row_names(input.rows());
+		for (size_t i = 0; i < input.rows(); ++i) {
+			row_names[i] = std::to_string(i);
+		}
+		std::vector<std::string> col_names(input.columns());
+		for (size_t i = 0; i < input.columns(); ++i) {
+			col_names[i] = std::to_string(i);
+		}
+		return EXPMatrixWriter::storeMatrixData(fileName, 
+			row_names,
+			col_names,
+			input, skip);
     }
     
     static bool storeMatrixData(const char * fileName, std::vector<std::string> const & genes,
 			std::vector<std::string> const & samples, FloatType* vectors, 
-            int const & vectorSizeAligned, bool const & skip = EXP_SKIP_TWO_ROWS) {
-            return EXPMatrixWriter::storeMatrixData(std::string(fileName), genes, samples, vectors, vectorSizeAligned, skip);
+            size_t const & stride_bytes, bool const & skip = EXP_SKIP_TWO_ROWS) {
+            return EXPMatrixWriter::storeMatrixData(std::string(fileName), genes, samples, vectors, stride_bytes, skip);
         }
 
 	/*dump the matrix data.  matrix has 1 sample per row.  output has to be 1 gene per row (transposed).*/
 	static bool storeMatrixData(std::string const & fileName, std::vector<std::string> const & genes,
             std::vector<std::string> const & samples, FloatType* vectors, 
-            int const & vectorSizeAligned, bool const & skip = EXP_SKIP_TWO_ROWS) {
+            size_t const & stride_bytes, bool const & skip = EXP_SKIP_TWO_ROWS) {
 
         const char delim[] = "\t";
 
@@ -80,7 +102,7 @@ public:
         // write header (samples)
         // header has ID and ALIAS
         ofs << "Id" << delim << "Alias";
-        for (int i = 0; i < samples.size(); ++i) {
+        for (size_t i = 0; i < samples.size(); ++i) {
             ofs << delim << samples[i];
         }
         ofs << std::endl;
@@ -93,16 +115,17 @@ public:
         ofs.precision(std::numeric_limits<FloatType>::max_digits10);
 
         // now write all data. 
-        for (int i = 0; i < genes.size(); ++i) {
+        FloatType* row;
+        for (size_t i = 0; i < genes.size(); ++i) {
 
             ofs << genes[i] << delim << "---";
 
-            for (int j = 0, jj = i * vectorSizeAligned; 
-                j < samples.size(); ++j, ++jj) {
-                ofs << delim << vectors[jj];
+            row = reinterpret_cast<FloatType*>(reinterpret_cast<void *>(vectors) + i * stride_bytes);
+            for (size_t j = 0; j < samples.size(); ++j) {
+                ofs << delim << row[j];
             }
             // this writes in transposed way.  not using.
-            // for (int j = 0, jj = i; j < samples.size(); ++j, jj += vectorSizeAligned) {
+            // for (size_t j = 0, jj = i; j < samples.size(); ++j, jj += stride_bytes) {
             //     ofs << delim << vectors[jj];
             // }
             ofs << std::endl;
@@ -117,6 +140,69 @@ public:
         return true;
 
     }
+
+    static bool storeMatrixData(const char * fileName, std::vector<std::string> const & genes,
+			std::vector<std::string> const & samples, splash::ds::aligned_matrix<FloatType> const & input,
+            bool const & skip = EXP_SKIP_TWO_ROWS) {
+            return EXPMatrixWriter::storeMatrixData(std::string(fileName), genes, samples, input, skip);
+        }
+
+	/*dump the matrix data.  matrix has 1 sample per row.  output has to be 1 gene per row (transposed).*/
+	static bool storeMatrixData(std::string const & fileName, std::vector<std::string> const & genes,
+            std::vector<std::string> const & samples, splash::ds::aligned_matrix<FloatType> const & input,
+            bool const & skip = EXP_SKIP_TWO_ROWS) {
+
+        const char delim[] = "\t";
+
+        auto stime = getSysTime();
+
+        fprintf(stderr, "EXPMatrixWriter writing r %lu x c %lu\n", genes.size(), samples.size() );
+        // open file
+        std::ofstream ofs;
+        ofs.open(fileName);
+        if (! ofs.is_open()) {
+            fprintf(stderr, "Failed to open file %s\n", fileName.c_str());
+            return false;
+        }
+
+        // write header (samples)
+        // header has ID and ALIAS
+        ofs << "Id" << delim << "Alias";
+        for (size_t i = 0; i < samples.size(); ++i) {
+            ofs << delim << samples[i];
+        }
+        ofs << std::endl;
+
+        // if skip, insert 2 empty lines
+        if (skip) {
+            ofs << "empty" << std::endl << "empty" << std::endl;
+        }
+        
+        ofs.precision(std::numeric_limits<FloatType>::max_digits10);
+
+        // now write all data. 
+        FloatType * row;
+        for (size_t i = 0; i < genes.size(); ++i) {
+
+            ofs << genes[i] << delim << "---";
+
+            row = input.data(i);
+            for (size_t j = 0; j < col_names.size(); ++j) {
+                ofs << delim << row[j];
+            }
+            ofs << std::endl;
+        }
+
+
+        ofs.close();
+
+        auto etime = getSysTime();
+        fprintf(stderr, "Wrote file %s in %f sec\n", fileName.c_str(), get_duration_s(stime, etime));
+
+        return true;
+
+    }
+
 };
 
 
--- include/io/MatrixWriter.hpp
+++ include/io/MatrixWriter.hpp
@@ -17,8 +17,8 @@
 #include <string>
 #include <limits>
 
-#include "benchmark.hpp"
-
+#include "utils/benchmark.hpp"
+#include "ds/aligned_matrix.hpp"
 
 namespace splash { namespace io { 
 
@@ -28,13 +28,13 @@ class MatrixWriter {
 public:
 
     static bool storeMatrixData(const char * fileName, size_t const & rows, size_t const & cols, 
-        FloatType* vectors, int const & vectorSizeAligned) {
-            return MatrixWriter::storeMatrixData(std::string(fileName), rows, cols, vectors, vectorSizeAligned);
-        }
+        FloatType* vectors, size_t const & stride_bytes) {
+        return MatrixWriter::storeMatrixData(std::string(fileName), rows, cols, vectors, stride_bytes);
+    }
 
 
     static bool storeMatrixData(std::string const & fileName, size_t const & rows, size_t const & cols, 
-        FloatType* vectors, int const & vectorSizeAligned) {
+        FloatType* vectors, size_t const & stride_bytes) {
         
 		std::vector<std::string> row_names(rows);
 		for (size_t i = 0; i < rows; ++i) {
@@ -47,19 +47,38 @@ public:
 		return MatrixWriter::storeMatrixData(fileName, 
 			row_names,
 			col_names,
-			vectors, vectorSizeAligned);
+			vectors, stride_bytes);
     }
-    
+
+    static bool storeMatrixData(const char * fileName, splash::ds::aligned_matrix<FloatType> const & input) {
+        return MatrixWriter::storeMatrixData(std::string(fileName), input);
+    }
+    static bool storeMatrixData(std::string const & fileName, splash::ds::aligned_matrix<FloatType> const & input) {
+        
+		std::vector<std::string> row_names(input.rows());
+		for (size_t i = 0; i < input.rows(); ++i) {
+			row_names[i] = std::to_string(i);
+		}
+		std::vector<std::string> col_names(input.columns());
+		for (size_t i = 0; i < input.columns(); ++i) {
+			col_names[i] = std::to_string(i);
+		}
+		return MatrixWriter::storeMatrixData(fileName, 
+			row_names,
+			col_names,
+			input);
+    }
+
     static bool storeMatrixData(const char * fileName, std::vector<std::string> const & row_names,
 			std::vector<std::string> const & col_names, FloatType* vectors, 
-            int const & vectorSizeAligned) {
-            return MatrixWriter::storeMatrixData(std::string(fileName), row_names, col_names, vectors, vectorSizeAligned);
+            size_t const & stride_bytes) {
+            return MatrixWriter::storeMatrixData(std::string(fileName), row_names, col_names, vectors, stride_bytes);
         }
 
 	/*dump the matrix data.  matrix in row-major.  output is 1 line per row.*/
 	static bool storeMatrixData(std::string const & fileName, std::vector<std::string> const & row_names,
 			std::vector<std::string> const & col_names, FloatType* vectors, 
-            int const & rowStride) {
+            size_t const & stride_bytes) {
 
         const char delim[] = ",";
 
@@ -75,7 +94,7 @@ public:
 
         // write header (samples)
         // empty first entry.
-        for (int i = 0; i < col_names.size(); ++i) {
+        for (size_t i = 0; i < col_names.size(); ++i) {
             ofs << delim << col_names[i];
         }
         ofs << std::endl;
@@ -83,12 +102,13 @@ public:
         ofs.precision(std::numeric_limits<FloatType>::max_digits10);
 
         // now write all data.
-        for (int i = 0; i < row_names.size(); ++i) {
+        FloatType* row;
+        for (size_t i = 0; i < row_names.size(); ++i) {
             ofs << row_names[i];
 
-            for (int j = 0, jj = i * rowStride; 
-                j < col_names.size(); ++j, ++jj) {
-                ofs << delim << vectors[jj];
+            row = reinterpret_cast<FloatType*>(reinterpret_cast<void *>(vectors) + i * stride_bytes);
+            for (size_t j = 0; j < col_names.size(); ++j) {
+                ofs << delim << row[j];
             }
             ofs << std::endl;
         }
@@ -101,6 +121,58 @@ public:
         return true;
 
     }
+
+    static bool storeMatrixData(const char * fileName, std::vector<std::string> const & row_names,
+			std::vector<std::string> const & col_names, splash::ds::aligned_matrix<FloatType> const & input) {
+            return MatrixWriter::storeMatrixData(std::string(fileName), row_names, col_names, input);
+        }
+
+	/*dump the matrix data.  matrix in row-major.  output is 1 line per row.*/
+	static bool storeMatrixData(std::string const & fileName, std::vector<std::string> const & row_names,
+			std::vector<std::string> const & col_names, splash::ds::aligned_matrix<FloatType> const & input) {
+        
+        const char delim[] = ",";
+
+        auto stime = getSysTime();
+
+        // open file
+        std::ofstream ofs;
+        ofs.open(fileName);
+        if (! ofs.is_open()) {
+            fprintf(stderr, "Failed to open file %s\n", fileName.c_str());
+            return false;
+        }
+
+        // write header (samples)
+        // empty first entry.
+        for (size_t i = 0; i < col_names.size(); ++i) {
+            ofs << delim << col_names[i];
+        }
+        ofs << std::endl;
+
+        ofs.precision(std::numeric_limits<FloatType>::max_digits10);
+
+        // now write all data.
+        FloatType * row;
+        for (size_t i = 0; i < row_names.size(); ++i) {
+            ofs << row_names[i];
+
+            row = input.data(i);
+            for (size_t j = 0; j < col_names.size(); ++j) {
+                ofs << delim << row[j];
+            }
+            ofs << std::endl;
+        }
+
+
+        ofs.close();
+        auto etime = getSysTime();
+        fprintf(stderr, "Wrote file %s in %f sec\n", fileName.c_str(), get_duration_s(stime, etime));
+
+        return true;
+
+    }
+
 };
 
 
