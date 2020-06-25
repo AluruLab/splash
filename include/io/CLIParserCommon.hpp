35ba736b6f66ba99032bd63664b733e539f80530
Tony Pan <tcp1975@gmail.com>
2020-06-25 14:13:13 -0400

    ENH: reorganized cli parameters, and force choice of inputfile vs random input

--- include/io/CLIParserCommon.hpp
+++ include/io/CLIParserCommon.hpp
@@ -33,12 +33,6 @@ class mpi_parameters : public parameters_base {
 	        MPI_Init(&argc, &argv);
             MPI_Comm_rank(MPI_COMM_WORLD, &rank);
         	MPI_Comm_size(MPI_COMM_WORLD, &procs);
-
-            PRINT("Warming up MPI with %d procs\n", procs);
-
-            int * data = (int*)malloc(procs * sizeof(int));
-            MPI_Alltoall(MPI_IN_PLACE, 1, MPI_INT, data, 1, MPI_INT, MPI_COMM_WORLD);
-            free(data);
 #endif
             }
         virtual ~mpi_parameters() {
@@ -47,7 +41,15 @@ class mpi_parameters : public parameters_base {
 #endif
         }
 
-        virtual void config(CLI::App& app) {}
+        virtual void config(CLI::App& app) {
+#ifdef USE_MPI
+            PRINT("Config: warm up MPI with %d procs\n", procs);
+
+            int * data = (int*)malloc(procs * sizeof(int));
+            MPI_Alltoall(MPI_IN_PLACE, 1, MPI_INT, data, 1, MPI_INT, MPI_COMM_WORLD);
+            free(data);
+#endif
+        }
 
         virtual void print() {
             PRINT("Number of MPI processes: %d\n", procs);
@@ -60,15 +62,15 @@ class common_parameters : public parameters_base {
         std::string output;
         bool random;
         // bool use_single;   // use double only, for now.
-        long num_vectors;
-        long vector_size;
+        size_t num_vectors;
+        size_t vector_size;
         size_t num_threads;
         long rseed;
         double rmin;
         double rmax;
 
         common_parameters() :
-            input(""), random(true), // use_single(true),
+            input(""), random(false), // use_single(true),
             num_vectors(0), vector_size(0), 
             num_threads(1), rseed(11), rmin(0.0), rmax(1.0) {}
         virtual ~common_parameters() {}
@@ -78,30 +80,37 @@ class common_parameters : public parameters_base {
         // actual parsing should also happen there.
         virtual void config(CLI::App& app) {
             // allow input, or random input, or piped input later. 
-            auto input_opt = app.add_option("-i,--input", input, "input EXP formatted file")->group("common")->check(CLI::ExistingFile);
-			app.add_option("-o,--output", output, "output file")->group("common");
+            auto input_opt = app.add_option("-i,--input", input, "input EXP formatted file")->check(CLI::ExistingFile);
+            auto random_opt = app.add_flag("-r,--random", random, "generate random data");
 
-            auto random_opt = app.add_flag("-r,--random", random, "generate random data")->group("common");
-            input_opt->excludes(random_opt);
-            random_opt->excludes(input_opt);
+            // require either -i input, or -r random.
+            auto opt_group = app.add_option_group("Input Source");
+            opt_group->add_option(input_opt);
+            opt_group->add_option(random_opt);
+            opt_group->require_option(1);
 
-            auto seed_opt = app.add_option("--random-seed", rseed, "random number generator seed")->group("common");
-            auto min_opt = app.add_option("--random-min", rmin, "random number min")->group("common");
-            auto max_opt = app.add_option("--random-max", rmax, "random number max")->group("common");
+            // this should be required for random and optional for input file.
+            auto nvec_opt = app.add_option("-n,--num-vectors", num_vectors, "number of samples")->group("Data")->check(CLI::PositiveNumber);
+            auto vsize_opt = app.add_option("-v,--vector-size", vector_size, "number of variables per sample")->group("Data")->check(CLI::PositiveNumber);
+            random_opt->needs(nvec_opt);
+            random_opt->needs(vsize_opt);
+
+
+            auto seed_opt = app.add_option("--random-seed", rseed, "random number generator seed")->group("RNG");
+            auto min_opt = app.add_option("--random-min", rmin, "random number min")->group("RNG");
+            auto max_opt = app.add_option("--random-max", rmax, "random number max")->group("RNG");
             seed_opt->needs(random_opt);
             min_opt->needs(random_opt);
             max_opt->needs(random_opt);
 
-            auto nvec_opt = app.add_option("-n,--num-vectors", num_vectors, "number of samples")->group("common")->check(CLI::PositiveNumber);
-            auto vsize_opt = app.add_option("-v,--vector-size", vector_size, "number of variables per sample")->group("common")->check(CLI::PositiveNumber);
-            random_opt->needs(nvec_opt);
-            random_opt->needs(vsize_opt);
+            // output
+            app.add_option("-o,--output", output, "output file")->group("Output");
 
             // must explicitly specify to use single precision.
             // app.add_flag("-s,--single", use_single, "use single precision")->group("common");
 
             // default to 1 thread.
-            app.add_option("-t,--threads", num_threads, "number of CPU threads")->group("common")->check(CLI::PositiveNumber);
+            app.add_option("-t,--threads", num_threads, "number of CPU threads")->group("Hardware")->check(CLI::PositiveNumber);
 
             // MPI parameters can be extracted from MPI runtime.
         }
