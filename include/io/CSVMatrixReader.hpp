77b91844ae0a76301e685b7f3042638337576f5c
Tony Pan <tpan7@gatech.edu>
2020-08-20 17:21:32 -0400

    FIX: wrong string contructor, causing implicit string construction from entire file content.  ENH: added tokenizer that uses single char delimiter instead of set of delimiters.

--- include/io/CSVMatrixReader.hpp
+++ include/io/CSVMatrixReader.hpp
@@ -10,7 +10,6 @@
 
 #pragma once
 
-#include <io/CustomFileReader.hpp>
 #include <string>
 #include <algorithm>
 #include <vector>
@@ -18,6 +17,11 @@
 #include <sys/mman.h>  // mmap
 #include <fcntl.h>  // open
 #include <sys/stat.h> //stat
+#include <iostream>
+#include <fstream>
+
+#include "utils/benchmark.hpp"
+#include "utils/report.hpp"
 
 #ifdef USE_MPI
 #include <mpi.h>
@@ -36,15 +40,33 @@ class FileReader {
 		// strtok modifies the buffer in memory.  avoid.
 
 		static const char EOL[];
-		static const char CR;
-		static const char COMMA[];
-		static const char TAB[];
+		static constexpr char CR = '\r';
+		static constexpr char COMMA = ',';
 		static constexpr size_t EMPTY = 0;
 
 		char_array data;
 		bool mapped;
 
-		inline char_array open(const char * filename) {
+		inline char_array load(const char * filename) {
+			auto stime = getSysTime();
+
+			char_array output = {nullptr, 0};
+			
+			std::ifstream file(filename, std::ios::binary | std::ios::in | std::ios::ate);
+			output.size = file.tellg();
+			file.seekg(0, std::ios::beg);
+
+			output.start = reinterpret_cast<char *>(splash::utils::aalloc(output.size));
+			file.read(output.start, output.size);
+
+			auto etime = getSysTime();
+			ROOT_PRINT("read file in %f sec\n", get_duration_s(stime, etime));
+			return output;
+		}
+
+
+		inline char_array map(const char * filename) {
+			auto stime = getSysTime();
 			char_array output = {nullptr, 0};
 
 			int fd = ::open(filename, O_RDONLY);
@@ -63,11 +85,12 @@ class FileReader {
 					output.start = nullptr;
 				}
 			}
+			auto etime = getSysTime();
+			ROOT_PRINT("map file in %f sec\n", get_duration_s(stime, etime));
 			return output;
-
 		}
 
-		inline void close(char_array & buffer) {
+		inline void unmap(char_array & buffer) {
 #ifndef NDEBUG
 			int rc = 
 #endif
@@ -150,13 +173,16 @@ class FileReader {
 
 #ifdef USE_MPI
 		// memmap the whole file on rank 1 only  NOT using MPI_File
-		FileReader(const char * filename, MPI_Comm comm = MPI_COMM_WORLD) {
+		FileReader(const char * filename, MPI_Comm comm = MPI_COMM_WORLD, bool _map = true) {
 			int rank;
 			MPI_Comm_rank(comm, &rank);
 
 			if (rank == 0) {  // rank 0 read the file and broadcast.
-				data = this->open(filename); 
-				mapped = true;
+				if (_map)
+					data = this->map(filename);
+				else
+					data = this->load(filename);			
+				mapped = _map;
 				MPI_Bcast(&(data.size), 1, MPI_UNSIGNED_LONG, 0, comm);
 				MPI_Bcast(data.start, data.size, MPI_BYTE, 0, comm);
 			} else {
@@ -169,13 +195,16 @@ class FileReader {
 		}
 #else
 		// memmap the whole file
-		FileReader(const char * filename) : mapped(true) {
-			data = this->open(filename);
+		FileReader(const char * filename, bool _map = true) : mapped(_map) {
+			if (_map)
+				data = this->map(filename);
+			else
+				data = this->load(filename);			
 		}
 #endif
 		virtual ~FileReader() {
 			if (mapped && (data.start != nullptr) && (data.size != 0)) {
-				this->close(data);
+				this->unmap(data);
 			} else if (!mapped && (data.start != nullptr)) {
 				splash::utils::afree(data.start);
 			}
@@ -186,6 +215,8 @@ class FileReader {
 		// search buffer to get to the first non-delim character, and return size.
 		template <size_t N>
 		char_array trim_left(char_array & buffer, const char (&delim)[N]) {
+			// auto stime = getSysTime();	
+
 			char_array output = {buffer.start, EMPTY};
 			if ((buffer.size == EMPTY) || (buffer.start == nullptr)) return output;  // buffer is empty, short circuit.
 			auto delim_end = delim + N;
@@ -194,11 +225,44 @@ class FileReader {
 			buffer.start = std::find_if(buffer.start, buffer.start + buffer.size,
 				[&delim, &delim_end](char const & c) {
 					// by failing to find a match
-					return (std::find(delim, delim_end, c) == delim_end);
+					for (size_t i = 0; i < N; ++i) {
+						if (delim[i] == c) return false;
+					}
+					return true;
+					// return (std::find(delim, delim_end, c) == delim_end);
 				});
 			
 			output.size = std::distance(output.start, buffer.start);
 			buffer.size -= output.size;
+
+			// auto etime = getSysTime();
+			// ROOT_PRINT("trim_left x in %f sec\n", get_duration_s(stime, etime));
+			return output;
+		}
+		char_array trim_left(char_array & buffer, const char delim) {
+			// auto stime = getSysTime();	
+			char_array output = {buffer.start, EMPTY};
+			if ((buffer.size == EMPTY) || (buffer.start == nullptr)) return output;  // buffer is empty, short circuit.
+
+			// find first entry that is NOT in delim
+			size_t i;
+			for (i = 0; i < buffer.size; ++i) {
+				if (buffer.start[i] != delim) break;
+			}
+			buffer.start += i;
+			output.size = i;
+			buffer.size -= i;
+
+			// buffer.start = std::find_if(buffer.start, buffer.start + buffer.size,
+			// 	[&delim](char const & c) {
+			// 		// by failing to find a match
+			// 		return delim != c;
+			// 	});
+			// output.size = std::distance(output.start, buffer.start);
+			// buffer.size -= output.size;
+
+			// auto etime = getSysTime();	
+			// ROOT_PRINT("trim_left in %f sec\n", get_duration_s(stime, etime));
 			return output;
 		}
 
@@ -208,6 +272,7 @@ class FileReader {
 		// if there is no more token, then return nullptr
 		template <size_t N>
 		char_array extract_token(char_array & buffer, const char (&delim)[N]) {
+			// auto stime = getSysTime();	
 			if ((buffer.size == EMPTY) || (buffer.start == nullptr)) return char_array{nullptr, EMPTY};  // buffer is empty, short circuit.
 			char_array output = {buffer.start, EMPTY};
 			
@@ -217,19 +282,53 @@ class FileReader {
 			buffer.start = std::find_if(buffer.start, buffer.start + buffer.size,
 			[&delim, &delim_end](char const & c){
 				// by finding a match
-				return (std::find(delim, delim_end, c) != delim_end);
+				for (size_t i = 0; i < N; ++i) {
+					if (delim[i] == c) return true;
+				}
+				return false;
+				// return (std::find(delim, delim_end, c) != delim_end);
 			});
 
 			// update the output and buffer sizes.
 			output.size = std::distance(output.start, buffer.start);
 			buffer.size -= output.size;
-
+			// printf("extract_token: %lu\n", output.size);
+			// auto etime = getSysTime();	
+			// ROOT_PRINT("extract token x in %f sec\n", get_duration_s(stime, etime));
 			return output;
 		}
+		char_array extract_token(char_array & buffer, const char delim) {
+			// auto stime = getSysTime();	
+			if ((buffer.size == EMPTY) || (buffer.start == nullptr)) return char_array{nullptr, EMPTY};  // buffer is empty, short circuit.
+			char_array output = {buffer.start, EMPTY};
+			
+			// search for first match to delim
+			size_t i;
+			for (i = 0; i < buffer.size; ++i) {
+				if (buffer.start[i] == delim) break;
+			}
+			buffer.start += i;
+			output.size = i;
+			buffer.size -= i;
 
+			// buffer.start = std::find_if(buffer.start, buffer.start + buffer.size,
+			// [&delim](char const & c){
+			// 	return (delim == c);
+			// 	// return (std::find(delim, delim_end, c) != delim_end);
+			// });
+			// // update the output and buffer sizes.
+			// output.size = std::distance(output.start, buffer.start);
+			// buffer.size -= output.size;
+			// printf("extract_token: %lu\n", output.size);
+			// auto etime = getSysTime();	
+			// ROOT_PRINT("extract token in %f sec\n", get_duration_s(stime, etime));
+			return output;
+		}
 		// remove 1 delim character from buffer (or \r\n)
 		template <size_t N>
 		char_array trim_left_1(char_array & buffer, const char (&delim)[N]) {
+			// auto stime = getSysTime();	
+
 			char_array output = {buffer.start, EMPTY};
 			if ((buffer.size == EMPTY) || (buffer.start == nullptr)) return output;  // buffer is empty, short circuit.
 			
@@ -246,14 +345,36 @@ class FileReader {
 				++buffer.start;
 			}
 			output.size = std::distance(output.start, buffer.start);
+			// printf("trim_left_1: %lu\n", output.size);
+			// auto etime = getSysTime();	
+			// ROOT_PRINT("trim_left_1 x in %f sec\n", get_duration_s(stime, etime));
 			return output;
 		}
-
+		char_array trim_left_1(char_array & buffer, const char delim) {
+			// auto stime = getSysTime();	
+			char_array output = {buffer.start, EMPTY};
+			if ((buffer.size == EMPTY) || (buffer.start == nullptr)) return output;  // buffer is empty, short circuit.
+			
+			if ((buffer.size > EMPTY) && (buffer.start[0] == delim)) {
+				--buffer.size;	
+				++buffer.start;
+			}
+			output.size = std::distance(output.start, buffer.start);
+			// printf("trim_left_1: %lu\n", output.size);
+			// auto etime = getSysTime();	
+			// ROOT_PRINT("trim_left_1 in %f sec\n", get_duration_s(stime, etime));
+			return output;
+		}
 
 	public:
 		// get a token. does not treat consecutive delimiters as one so output may be empty.
 		template <size_t N>
 		char_array get_token_or_empty(char_array & buffer, const char (&delim)[N]) {
+			char_array output = extract_token(buffer, delim);
+			trim_left_1(buffer, delim);
+			return output;
+		}
+		char_array get_token_or_empty(char_array & buffer, const char delim) {
 			char_array output = extract_token(buffer, delim);
 			trim_left_1(buffer, delim);
 			return output;
@@ -264,22 +385,56 @@ class FileReader {
 			char_array output = extract_token(buffer, delim);
 			trim_left(buffer, delim);
 			return output;
+		}		// return token (ptr and length). treats consecutive delimiters as one.
+		char_array get_token(char_array & buffer, const char delim) {
+			char_array output = extract_token(buffer, delim);
+			trim_left(buffer, delim);
+			return output;
 		}
 
 		template <size_t N>
 		size_t count_token_or_empty(char_array & buffer, const char (&delim)[N]) {
+			// auto stime = getSysTime();
 			if ((buffer.size == EMPTY) || (buffer.start == nullptr)) return 0;
 
 			auto delim_end = delim + N;
 			// now count delimiters
-			return std::count_if(buffer.start, buffer.start + buffer.size,
+			size_t count = std::count_if(buffer.start, buffer.start + buffer.size,
 				[&delim, &delim_end](const char & c){
-					return (std::find(delim, delim_end, c) != delim_end);
+					for (size_t i = 0; i < N; ++i) {
+						if (delim[i] == c) return true;
+					}
+					return false;
+					// return (std::find(delim, delim_end, c) != delim_end);
 				}) + 1;
-		}
 
+			// auto etime = getSysTime();
+			// ROOT_PRINT("count tokens or empty in %f sec\n", get_duration_s(stime, etime));
+			return count;
+		}
+		size_t count_token_or_empty(char_array & buffer, const char delim) {
+			// auto stime = getSysTime();
+			if ((buffer.size == EMPTY) || (buffer.start == nullptr)) return 0;
+
+			// now count delimiters
+			size_t count = 0;
+			for (size_t i = 0; i < buffer.size; ++i) {
+				if (buffer.start[i] == delim) ++count;
+			}
+			++count;
+			// size_t count = std::count_if(buffer.start, buffer.start + buffer.size,
+			// 	[&delim](const char & c){
+			// 		return (delim == c);
+			// 		// return (std::find(delim, delim_end, c) != delim_end);
+			// 	}) + 1;
+
+			// auto etime = getSysTime();
+			// ROOT_PRINT("count tokens or empty in %f sec\n", get_duration_s(stime, etime));
+			return count;
+		}
 		template <size_t N>
 		size_t count_token(char_array & buffer, const char (&delim)[N]) {
+			// auto stime = getSysTime();
 			if ((buffer.size == EMPTY) || (buffer.start == nullptr)) return 0;
 
 			size_t count = 0;
@@ -287,8 +442,14 @@ class FileReader {
 			// set up the adjacent find predicate
 			auto delim_end = delim + N;
 			auto adj_find_pred = [&delim, &delim_end](const char & x, const char & y){
-				bool x_no_delim = std::find(delim, delim_end, x) == delim_end;
-				bool y_delim = std::find(delim, delim_end, y) != delim_end;
+				// bool x_no_delim = std::find(delim, delim_end, x) == delim_end;
+				// bool y_delim = std::find(delim, delim_end, y) != delim_end;
+				bool x_no_delim = true;
+				bool y_delim = false;
+				for (size_t i = 0; i < N; ++i) {
+					x_no_delim &= (delim[i] != x);
+					y_delim |= (delim[i] == y);
+				}
 				return x_no_delim && y_delim; 
 			};
 
@@ -302,17 +463,63 @@ class FileReader {
 			}
 
 			// check the last char to see if we we have a non-delim to null transition.
-			if (std::find(delim, delim_end, buffer.start[buffer.size - 1]) == delim_end) {
-				++count;
+			bool z_no_delim = true;
+			for (size_t i = 0; i < N; ++i) {
+				z_no_delim &= (delim[i] != buffer.start[buffer.size - 1]);
 			}
+			count += z_no_delim;
+			
+			// if (std::find(delim, delim_end, buffer.start[buffer.size - 1]) == delim_end) {
+			// 	++count;
+			// }
+			// auto etime = getSysTime();
+			// ROOT_PRINT("count tokens in %f sec\n", get_duration_s(stime, etime));
 			return count;
 		}
 
+		size_t count_token(char_array & buffer, const char delim) {
+			// auto stime = getSysTime();
+			if ((buffer.size == EMPTY) || (buffer.start == nullptr)) return 0;
+
+			size_t count = 0;
+
+			// set up the adjacent find predicate
+			// auto adj_find_pred = [&delim](const char & x, const char & y){
+			// 	return (x != delim) && (y == delim); 
+			// };
+
+			// now count non-delimiter to delimiter transitions
+			// auto start = buffer.start;
+			// auto end = buffer.start + buffer.size;
+			// while ((start = std::adjacent_find(start, end, adj_find_pred)) != end) {
+			// 	// found a transition
+			// 	++start; // go to next char
+			// 	++count; // increment count
+			// }
+			// check the last char to see if we we have a non-delim to null transition.
+			// bool z_no_delim = true;
+			// for (size_t i = 0; i < N; ++i) {
+			// 	z_no_delim &= (delim[i] != buffer.start[buffer.size - 1]);
+			// }
+			// count += z_no_delim;
+
+
+			for (size_t i = 1; i < buffer.size; ++i) {
+				if ((buffer.start[i-1] != delim) && (buffer.start[i] == delim)) ++count;
+			}
+			count += (buffer.start[buffer.size - 1] != delim);
+			
+			// if (std::find(delim, delim_end, buffer.start[buffer.size - 1]) == delim_end) {
+			// 	++count;
+			// }
+			// auto etime = getSysTime();
+			// ROOT_PRINT("count tokens in %f sec\n", get_duration_s(stime, etime));
+			return count;
+		}
+
+
 };
 const char FileReader::EOL[] = "\r\n";
-const char FileReader::CR = '\r';
-const char FileReader::COMMA[] = ",";
-const char FileReader::TAB[] = "\t";
 
 template<typename FloatType>
 class CSVMatrixReader : public FileReader {
@@ -396,6 +603,7 @@ protected:
 template<typename FloatType>
 bool CSVMatrixReader<FloatType>::getMatrixSize_impl(
 		int& numVectors, int& vectorSize) {
+	auto stime = getSysTime();
 
 	char_array buffer = this->data;
 	if (buffer.start == nullptr) {
@@ -412,7 +620,7 @@ bool CSVMatrixReader<FloatType>::getMatrixSize_impl(
 	char_array line = this->get_token(buffer, FileReader::EOL);
 	if (line.size <= 0) {
 		fprintf(stderr, "The file is incomplete\n");
-		this->close(buffer);
+		this->unmap(buffer);
 		return false;
 	}
 
@@ -424,6 +632,8 @@ bool CSVMatrixReader<FloatType>::getMatrixSize_impl(
 	numVectors = FileReader::count_token(buffer, FileReader::EOL);
 	fprintf(stderr, "Number of gene expression profiles: %d\n", numVectors);
 
+	auto etime = getSysTime();
+	ROOT_PRINT("get matrix size in %f sec\n", get_duration_s(stime, etime));
 	return true;
 }
 
@@ -432,6 +642,7 @@ bool CSVMatrixReader<FloatType>::loadMatrixData_impl(
 		vector<string>& genes, vector<string>& samples, FloatType* vectors,
 		const int & numVectors, const int & vectorSize, 
 		const int & stride_bytes) {
+	auto stime = getSysTime();
 
 	char_array buffer = this->data;
 	char_array line, token;
@@ -444,14 +655,17 @@ bool CSVMatrixReader<FloatType>::loadMatrixData_impl(
 		fprintf(stderr, "The file is incomplete\n");
 		return false;
 	}
+	auto etime = getSysTime();
+	ROOT_PRINT("load 1st line in %f sec\n", get_duration_s(stime, etime));
 
+	stime = getSysTime();
 	/*analyze the header.  first entry is skipped.  save the sample names */
 	int numSamples = 0;
 	token = this->get_token_or_empty(line, FileReader::COMMA);  // skip first one.  this is column name
 	token = this->get_token_or_empty(line, FileReader::COMMA);  
 	for (; (token.start != nullptr) && (numSamples < vectorSize); 
 		token = this->get_token_or_empty(line, FileReader::COMMA), ++numSamples) {
-		samples.push_back(std::string(token.start, 0, token.size));
+		samples.emplace_back(std::string(token.start, token.size));
 	}
 	/*check consistency*/
 	if (numSamples < vectorSize) {
@@ -460,7 +674,10 @@ bool CSVMatrixReader<FloatType>::loadMatrixData_impl(
 				numSamples, vectorSize);
 		return false;
 	}
+	etime = getSysTime();
+	ROOT_PRINT("parse column headers %d in %f sec\n", numSamples, get_duration_s(stime, etime));
 
+	stime = getSysTime();
 
 	/*get gene expression profiles*/
 	/*extract gene expression values*/  // WAS READING TRANSPOSED.  NO LONGER.
@@ -475,7 +692,7 @@ bool CSVMatrixReader<FloatType>::loadMatrixData_impl(
 
 		// parse the row name
 		token = this->get_token_or_empty(line, FileReader::COMMA);
-		genes.push_back(std::string(token.start, 0, token.size));
+		genes.emplace_back(std::string(token.start, token.size));
 
 		// parse the rest of data.
 		vec = reinterpret_cast<FloatType*>(reinterpret_cast<unsigned char *>(vectors) + numGenes * stride_bytes);
@@ -483,7 +700,7 @@ bool CSVMatrixReader<FloatType>::loadMatrixData_impl(
 		token = this->get_token_or_empty(line, FileReader::COMMA);		  
 		for (; (token.start != nullptr) && (numSamples < vectorSize); 
 			token = this->get_token_or_empty(line, FileReader::COMMA), ++numSamples, ++vec) {
-			std::string s(token.start, 0, token.size);
+			std::string s(token.start, token.size);
 			*(vec) = atof(s.c_str()); // will read until a non-numeric char is encountered.
 		}
 		// NOTE: missing entries are treated as 0.
@@ -495,6 +712,8 @@ bool CSVMatrixReader<FloatType>::loadMatrixData_impl(
 				numGenes, numVectors);
 		return false;
 	}
+	etime = getSysTime();
+	ROOT_PRINT("load values in %f sec\n", get_duration_s(stime, etime));
 
 	return true;
 }
